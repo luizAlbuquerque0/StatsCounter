fab704cb820aa0494074a9002549bb632bd11ca0
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-01-02 18:03:24 -0300

    Implement Entities

---
+++ StatsCounter.Core/Entities/Match.cs
@@ -0,0 +1,19 @@
+﻿namespace StatsCounter.Core.Entities
+{
+    public class Match : BaseEntity
+    {
+        public Match(int playerId, DateTime date)
+        {
+            PlayerId = playerId;
+            Date = date;
+        }
+
+        public int TouchDowns { get; private set; }
+        public int Interceptations { get; private set; }
+        public int CompletedPasses { get; private set; }
+        public int AttemptedPasses { get; private set; }
+        public int PassedYards { get; private set; }
+        public int PlayerId { get; private set; }
+        public DateTime Date { get; private set; }
+    }
+}
---
+++ StatsCounter.Core/Entities/Player.cs
@@ -0,0 +1,17 @@
+﻿namespace StatsCounter.Core.Entities
+{
+    public class Player : BaseEntity
+    {
+        public Player(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public List<Match> Matches { get; private set; }
+    }
+}

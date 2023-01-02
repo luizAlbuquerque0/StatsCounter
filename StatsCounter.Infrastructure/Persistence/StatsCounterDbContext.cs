38a570b5e3ae8783ca6d033e8b165c1a76ec8b77
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-01-02 18:13:17 -0300

    Implement DbContext

---
+++ StatsCounter.Infrastructure/Persistence/StatsCounterDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StatsCounter.Core.Entities;
+using System.Reflection;
+
+namespace StatsCounter.Infrastructure.Persistence
+{
+    public class StatsCounterDbContext : DbContext
+    {
+        public StatsCounterDbContext(DbContextOptions<StatsCounterDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Match> Matches { get; set; }
+        public DbSet<Player> Players { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}

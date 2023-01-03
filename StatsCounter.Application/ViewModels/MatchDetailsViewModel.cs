b7d9da3a0356af11e8e664b2728e2339c8e52ff4
Luiz Alberto <luiz.albuquerque99@gmail.com>
2023-01-03 09:28:31 -0300

    implement view models

---
+++ StatsCounter.Application/ViewModels/MatchDetailsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using StatsCounter.Core.Entities;
+
+namespace StatsCounter.Application.ViewModels
+{
+    public class MatchDetailsViewModel
+    {
+        public MatchDetailsViewModel(int id, int touchDowns, int interceptations, int completedPasses, int attemptedPasses, int passedYards, DateTime date, Player player)
+        {
+            Id = id;
+            TouchDowns = touchDowns;
+            Interceptations = interceptations;
+            CompletedPasses = completedPasses;
+            AttemptedPasses = attemptedPasses;
+            PassedYards = passedYards;
+            Date = date;
+            Player = player;
+        }
+
+        public int Id { get; private set; }
+        public int TouchDowns { get; private set; }
+        public int Interceptations { get; private set; }
+        public int CompletedPasses { get; private set; }
+        public int AttemptedPasses { get; private set; }
+        public int PassedYards { get; private set; }
+        public DateTime Date { get; private set; }
+        public Player Player { get; private set; }
+
+    }
+}
---
+++ StatsCounter.Application/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace StatsCounter.Application.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public ProfileViewModel(int playedMatchs, int totalTouchDowns, int totalInterceptations, int totalCompletedPasses, int totalAttemptedPasses, int totalPassedYards, List<MatchDetailsViewModel> matchs)
+        {
+            PlayedMatchs = playedMatchs;
+            TotalTouchDowns = totalTouchDowns;
+            TotalInterceptations = totalInterceptations;
+            TotalCompletedPasses = totalCompletedPasses;
+            TotalAttemptedPasses = totalAttemptedPasses;
+            TotalPassedYards = totalPassedYards;
+            Matchs = matchs;
+        }
+
+        public int PlayedMatchs { get; private set; }
+        public int TotalTouchDowns { get; private set; }
+        public int TotalInterceptations { get; private set; }
+        public int TotalCompletedPasses { get; private set; }
+        public int TotalAttemptedPasses { get; private set; }
+        public int TotalPassedYards { get; private set; }
+        public List<MatchDetailsViewModel> Matchs { get; set; }
+    }
+}

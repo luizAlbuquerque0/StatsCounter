8905057c91753e40d682ac1f616c303b18a6c270
Luiz Alberto <luiz.albuquerque99@gmail.com>
2023-01-03 08:47:26 -0300

    implement repositories interfaces

---
+++ StatsCounter.Core/Repositories/IMatchRepository.cs
@@ -0,0 +1,14 @@
+﻿using StatsCounter.Core.Entities;
+
+namespace StatsCounter.Core.Repositories
+{
+    public interface IMatchRepository
+    {
+        Task CreateMatchAsync(Match match);
+        Task<List<Match>> GetAllMatchesByPlayerIdAsync(int playerId);
+        Task DeleteMatchAsync(int matchId);
+        Task<Match> GetMByIdAsync(int matchId);
+        Task SaveChangesAsync();
+
+    }
+}
---
+++ StatsCounter.Core/Repositories/IPlayerRepository.cs
@@ -0,0 +1,11 @@
+﻿using StatsCounter.Core.Entities;
+
+namespace StatsCounter.Core.Repositories
+{
+    public interface IPlayerRepository
+    {
+        Task CreatePlayerAsync(Player player);
+        Task<Player> GetPlayerByEmailAndPasswordAsync(string email, string password);
+        Task<Player> GetUserByIdAsync(int id);
+    }
+}

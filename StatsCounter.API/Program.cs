7a865dab93cf7a236f04c5bf9b665f83dec808dc
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-01-03 18:09:46 -0300

    Initial Migration

--- StatsCounter.API/Program.cs
+++ StatsCounter.API/Program.cs
@@ -1,6 +1,20 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using StatsCounter.Application.Commands.CreatePlayer;
+using StatsCounter.Core.Repositories;
+using StatsCounter.Infrastructure.Persistence;
+using StatsCounter.Infrastructure.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var conectionString = builder.Configuration.GetConnectionString("StatsCounterCs");
+builder.Services.AddDbContext<StatsCounterDbContext>(options => options.UseSqlServer(conectionString));
+builder.Services.AddMediatR(typeof(CreatePlayerCommand));
+
+builder.Services.AddScoped<IPlayerRepository, PlayerRepository>();
+builder.Services.AddScoped<IMatchRepository, MatchRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ StatsCounter.Infrastructure/Persistence/Migrations/20230103210928_InitialMigration.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StatsCounter.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TouchDowns = table.Column<int>(type: "int", nullable: false),
+                    Interceptations = table.Column<int>(type: "int", nullable: false),
+                    CompletedPasses = table.Column<int>(type: "int", nullable: false),
+                    AttemptedPasses = table.Column<int>(type: "int", nullable: false),
+                    PassedYards = table.Column<int>(type: "int", nullable: false),
+                    PlayerId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Matches_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_PlayerId",
+                table: "Matches",
+                column: "PlayerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Matches");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}

394b7d82717f05f43f2231e3d8dae16e58c3175e
Luiz Alberto <luiz.albuquerque99@gmail.com>
2023-01-03 09:06:40 -0300

    implement repositories

---
+++ StatsCounter.Infrastructure/Persistence/Repositories/MatchRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StatsCounter.Core.Entities;
+using StatsCounter.Core.Repositories;
+
+namespace StatsCounter.Infrastructure.Persistence.Repositories
+{
+    public class MatchRepository : IMatchRepository
+    {
+        private readonly StatsCounterDbContext _dbContext;
+        public MatchRepository(StatsCounterDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task CreateMatchAsync(Match match)
+        {
+            await _dbContext.Matches.AddAsync(match);
+        }
+
+        public async Task DeleteMatchAsync(int matchId)
+        {
+            var match = await _dbContext.Matches.SingleOrDefaultAsync(m => m.Id == matchId);
+
+            _dbContext.Matches.Remove(match);
+        }
+
+        public async Task<List<Match>> GetAllMatchesByPlayerIdAsync(int playerId)
+        {
+            return await _dbContext.Matches
+                .Where(m=> m.PlayerId == playerId)
+                .ToListAsync();
+        }
+
+        public async Task<Match> GetMByIdAsync(int matchId)
+        {
+            return await _dbContext.Matches.Include(m => m.Player).SingleOrDefaultAsync(m => m.Id == matchId);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ StatsCounter.Infrastructure/Persistence/Repositories/PlayerRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StatsCounter.Core.Entities;
+using StatsCounter.Core.Repositories;
+
+namespace StatsCounter.Infrastructure.Persistence.Repositories
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+        private readonly StatsCounterDbContext _dbContext;
+        public PlayerRepository(StatsCounterDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task CreatePlayerAsync(Player player)
+        {
+            await _dbContext.Players.AddAsync(player);
+            await _dbContext.SaveChangesAsync();    
+        }
+
+        public async Task<Player> GetPlayerByEmailAndPasswordAsync(string email, string password)
+        {
+            return await _dbContext.Players.SingleOrDefaultAsync(p => p.Email == email && p.Password == password);
+        }
+
+        public async Task<Player> GetUserByIdAsync(int id)
+        {
+            return await _dbContext.Players.SingleOrDefaultAsync(p => p.Id == id);
+        }
+    }
+}

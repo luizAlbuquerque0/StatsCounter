d790d81f1f158ddba99add2127e8a681acbfddef
Luiz Alberto <luiz.albuquerque99@gmail.com>
2023-01-11 08:56:20 -0300

    implement match controllers

---
+++ StatsCounter.API/Controllers/MatchController.cs
@@ -0,0 +1,68 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using StatsCounter.Application.Commands.CreateMatch;
+using StatsCounter.Application.Commands.DeleteMatch;
+using StatsCounter.Application.Commands.UpdateMatch;
+using StatsCounter.Application.Queries.GetAllPlayerMatches;
+using StatsCounter.Application.Queries.GetMatchById;
+
+namespace StatsCounter.API.Controllers
+{
+    [Route("api/matchs")]
+    public class MatchController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public MatchController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var query = new GetMatchByIdQuery(id);
+
+            var match = await _mediator.Send(query);
+            if (match == null) return NotFound();
+
+            return Ok(match);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMatch([FromBody] CreateMatchCommand command)
+        {
+            var id = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetById), new { id = id }, command);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateMatch([FromBody] UpdateMatchCommand command)
+        {
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMatch(int id)
+        {
+            var command = new DeleteMatchCommand(id);
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpGet("player/{id}")]
+        public async Task<IActionResult> GetAllPlayerMatches(int id)
+        {
+            var query = new GetAllPlayerMatchesQuery(id);
+
+            var matchs = await _mediator.Send(query);
+            if (matchs == null) return NotFound();
+
+            return Ok(matchs);
+        }
+
+
+    }
+}

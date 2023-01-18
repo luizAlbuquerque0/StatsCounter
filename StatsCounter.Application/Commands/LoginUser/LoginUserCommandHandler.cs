9864085fe49dd3d9121eab196d149ad622a5a4b9
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-01-18 19:03:20 -0300

    implement login command

---
+++ StatsCounter.Application/Commands/LoginUser/LoginUserCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using StatsCounter.Application.ViewModels;
+using StatsCounter.Core.Repositories;
+
+namespace StatsCounter.Application.Commands.LoginUser
+{
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, LoginUserViewModel>
+    {
+        private readonly IPlayerRepository _playerRepository;
+        public LoginUserCommandHandler(IPlayerRepository playerRepository)
+        {
+            _playerRepository = playerRepository;
+        }
+        public async Task<LoginUserViewModel> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            var player = await _playerRepository.GetPlayerByEmailAndPasswordAsync(request.Email, request.Password);
+            if(player == null) return null;
+
+            return new LoginUserViewModel(player.Email, "token", player.Id);
+        }
+    }
+}

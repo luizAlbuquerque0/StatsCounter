07b98536e73cdae146c23d8132eb6f2ec075a011
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-01-30 15:48:35 -0300

    feature/implement-CreateMatch-UnitTest

---
+++ StatsCounter.UnitTests/Application/Commands/CreateMatchCommandHandlerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using StatsCounter.Application.Commands.CreateMatch;
+using StatsCounter.Core.Repositories;
+
+namespace StatsCounter.UnitTests.Application.Commands
+{
+    public class CreateMatchCommandHandlerTests
+    {
+        private readonly Mock<IMatchRepository> _matchRepositoryMock;
+        public CreateMatchCommandHandlerTests()
+        {
+            _matchRepositoryMock = new Mock<IMatchRepository>();
+        }
+        [Fact]
+        public async void InputDateIsOk_Executed_ReturnMatchId()
+        {
+
+            var createMatchCommand = new CreateMatchCommand(playerId: 1, date: DateTime.Now);
+
+            var createMatchCommandHandler = new CreateMatchCommandHandler(_matchRepositoryMock.Object);
+
+            var id = await createMatchCommandHandler.Handle(createMatchCommand, new CancellationToken());
+
+            Assert.True(id >= 0);
+        }
+    }
+}
